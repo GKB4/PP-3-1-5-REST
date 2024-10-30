d5bae627e37580b1236b82f81d66f7c1f1ddddf2
Igor Ilkov <garikb@inbox.ru>
2024-10-30 20:19:09 +0500

    Add clean ver.1

---
+++ src/main/java/kata/preproject/task3_1_3/Task313Application.java
@@ -0,0 +1,13 @@
+package kata.preproject.task3_1_3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Task313Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Task313Application.class, args);
+	}
+
+}
